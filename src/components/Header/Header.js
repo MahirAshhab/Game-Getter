ad2fbc6030942f8620c655d10eec836f0ae39ef2
MahirAshhab <mahir.ashhab77@gmail.com>
2022-01-20 02:08:47 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>GameGetter</h1>
+            <h3>HEART OF PERFECT GAMING</h3>
+            <p>Wallet: $100</p>
+        </div>
+    );
+};
+
+export default Header;
